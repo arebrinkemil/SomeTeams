5e3866211dcf218b1d7be602c5b61b1979b0372d
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2023-09-29 11:55:20 +0200

    created first files

---
+++ body.php
@@ -0,0 +1,22 @@
+<?php
+require __DIR__ . '/teams.php';
+require __DIR__ . '/data.php';
+?>
+
+<table>
+    <tr>
+        <th>Company</th>
+        <th>Contact</th>
+        <th>Country</th>
+    </tr>
+    <tr>
+        <td>Alfreds Futterkiste</td>
+        <td>Maria Anders</td>
+        <td>Germany</td>
+    </tr>
+    <tr>
+        <td>Centro comercial Moctezuma</td>
+        <td>Francisco Chang</td>
+        <td>Mexico</td>
+    </tr>
+</table>
