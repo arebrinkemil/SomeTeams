9c58c15200d5994c62eb5432ea6ec0a17010bb78
arebrinkemil <60137005+arebrinkemil@users.noreply.github.com>
2023-10-01 21:32:50 +0200

    added logo in head

--- head.php
+++ head.php
@@ -5,7 +5,12 @@
     <link rel="stylesheet" href="styles.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>
+        Some Teams //Emil Årebrink
+    </title>
+
+    <!-- add icon link -->
+    <link rel="icon" href="images/logga.png" type="image/x-icon">
 
 </head>
 
